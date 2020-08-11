8ea1aa42d2f8572c73bad3e8f45938742a95f91e
jkjkil4 <52841865+jkjkil4@users.noreply.github.com>
2020-08-11 12:44:20 +0800

    防多开

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include "mainwidget.h"
 
 #include <QApplication>
+#include <QSharedMemory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    MainWidget w;
+    QSharedMemory sharedMem;
+    sharedMem.setKey(QString("UsefulMenu_SharedMem"));
+    if(sharedMem.attach()){
+        QMessageBox::information(nullptr, "提示", "程序正在运行");
+        return 0;
+    }
 
-    return a.exec();
+    if(sharedMem.create(1)) {
+        MainWidget w;
+        return a.exec();
+    }
+
+    return 0;
 }
